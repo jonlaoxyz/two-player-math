ecd84e8790491d5ce181a5b6eb8c008b86ada303
Jonathan Lao <jon.lao@gmail.com>
2024-01-23 10:31:39 -0600

    fix typo in turn.rb

--- turn.rb
+++ turn.rb
@@ -7,7 +7,7 @@ class Turn
   end
 
   def run_turn
-    DisplayGame.display_message("#{player.name}: What does #{question.num1} plus #{quesiton.num2} equals?")
+    DisplayGame.display_message("#{player.name}: What does #{question.num1} plus #{question.num2} equals?")
     answer = DisplayGame.prompt_player_for_answer
     player.answer_question(answer)
   end
