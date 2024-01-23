e399161e798e8c6dbc843b02a6900fdcf3a23083
Jonathan Lao <jon.lao@gmail.com>
2024-01-23 10:27:59 -0600

    code display_game

--- display_game.rb
+++ display_game.rb
@@ -1,4 +1,11 @@
 class DisplayGame
-  puts "DisplayGame Class"
+  def self.prompt_player_for_answer
+    print "> "
+    gets.chomp.to_i
+  end
+
+  def self.display_message(message)
+    puts message
+  end
 
 end
--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
   require_relative 'player'
   require_relative 'game'
   require_relative 'question'
+  require_relative 'turn'
+  require_relative 'display_game'
 
 game = Game.new("Player 1", "Player 2")
 game.start_game
